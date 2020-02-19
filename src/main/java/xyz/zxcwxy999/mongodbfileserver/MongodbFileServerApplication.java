c961bb885fd6b2a2d2f30826e344563807c7e963
ZZZXXXCCCWXY999 <zzzxxxcccwxy999@gmail.com>
2020-02-19 17:45:26 +0800

    mvn compile

---
+++ src/main/java/xyz/zxcwxy999/mongodbfileserver/MongodbFileServerApplication.java
@@ -0,0 +1,13 @@
+package xyz.zxcwxy999.mongodbfileserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MongodbFileServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MongodbFileServerApplication.class, args);
+    }
+
+}
