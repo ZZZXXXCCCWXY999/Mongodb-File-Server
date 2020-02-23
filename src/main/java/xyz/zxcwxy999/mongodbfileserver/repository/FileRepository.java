97c03d35b11c45fc9beabadb3758d38957dde35f
ZZZXXXCCCWXY999 <zzzxxxcccwxy999@gmail.com>
2020-02-23 15:57:45 +0800

    修复了上传的文件不能删除的bug，修复了css、js文件无法引入的错误

--- src/main/java/xyz/zxcwxy999/mongodbfileserver/repository/FileRepository.java
+++
@@ -1,7 +0,0 @@
-package xyz.zxcwxy999.mongodbfileserver.repository;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-import xyz.zxcwxy999.mongodbfileserver.domain.File;
-
-public interface FileRepository extends MongoRepository<File,String> {
-}
